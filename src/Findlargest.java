48f28a20fd616c56fc11ba7abbd1cf8dc573c4e2
MarkHooks <hooksma174@gmail.com>
2019-10-22 13:37:28 -0400

    finished find largest

---
+++ src/Findlargest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Findlargest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int a = scan.nextInt();
+        System.out.println("Enter the second number: ");
+        int b = scan.nextInt();
+        if(a>b)
+            System.out.println(a+"is greater than"+b);
+        if(a<b)
+            System.out.println(a+"is less than"+ b);
+        if(a==b)
+            System.out.println(a+"is equal to"+b);
+
+    }
+
+}
